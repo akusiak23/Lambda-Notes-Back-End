4a4b1c2b433def3e5f45e74967e7ad454984bb45
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-29 14:50:21 -0500

    Helper functions created

---
+++ data/helper.js
@@ -0,0 +1,29 @@
+const db = require('./index');
+
+function getNotes() {
+    return db('notes');
+}
+
+function getNote(id) {
+    return db('notes').where({ id:id });
+}
+
+function addNote(note) {
+    return db('notes').insert(note);
+}
+
+function updateNote(id, note) {
+    return db('notes').where({ id: id }).update(note);
+}
+
+function deleteNote(id) {
+    return db('notes').where({ id: id }).del();
+}
+
+module.exports = {
+    getNotes,
+    getNote,
+    addNote,
+    updateNote,
+    deleteNote
+};
