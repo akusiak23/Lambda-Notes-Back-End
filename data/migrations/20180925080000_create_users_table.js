d7b43564dd0d2f294c889ab14bef12eb4474342b
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-11-01 11:17:35 -0500

    db fix

---
+++ data/migrations/20180925080000_create_users_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notes', function(table) {
+        table.increments();
+        table.string('title').notNullable();
+        table.string('content').notNullable();
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('notes');  
+  };
+  
